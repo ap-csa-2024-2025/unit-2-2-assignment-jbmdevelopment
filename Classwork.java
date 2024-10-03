f64ddf16e5b9c3281ba5378bafb749ebb6abf93b
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-10-03 05:30:20 +0000

    ezz

--- Classwork.java
+++ Classwork.java
@@ -10,6 +10,8 @@ public class Classwork
     int amount = sc.nextInt();
     System.out.println("How much does each one weigh?"); // 11, 3.75, 41.25
     double weight = sc.nextDouble();
-    System.out.println(item + "at" + weight +"pounds each will weigh" + (amount * weight)+ "pounds total");
+    System.out.println(item + " at " + weight + " pounds each will weigh " + (amount * weight) + " pounds total");
+    System.out.println("\"That brain of mine is something more than merely mortal; as time will show.\" \nAda Lovelace\nThe first computer programmer");
+    System.out.println("(\\(\\) \n(- -) \n((') ('))");
   }
 }
